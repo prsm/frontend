55704cb62060fc7c94bc22dc9d173ca7b1559956
callum stringer <callumstringer10@gmail.com>
2021-10-15 12:41:15 +0200

    fixed enddate bug, added time to event

--- data.js
+++ data.js
@@ -27,93 +27,16 @@ export default {
   ],
   events: [
     {
-      title: 'Lan A',
-      location: 'Zegline AMK',
+      title: 'Two of a Kind X Manabar',
+      location: 'Manabar Basel',
       description:
-        'Description is the pattern of narrative development that aims to make vivid a place, object, character, or group. Description is one of four rhetorical modes, along with exposition, argumentation, and narration. In practice it would be difficult to write literature that drew on just one of the four basic modes.',
+        'A Counter Strike - Global Offensive wingman tournament hosted at the Manabar in Basel',
       href: 'https://lanport.ch/lan2k22',
-      start: '1995-12-17T03:24:00',
-      end: '1995-12-17T03:24:00',
-      organiser: 'Lanport',
+      start: '2021-10-30T03:00:00',
+      end: '2021-11-30T03:00:00',
+      organiser: 'PR1SM',
     },
-    {
-      title: 'Lan B',
-      location: 'Zegline AMK',
-      description:
-        'Description is the pattern of narrative development that aims to make vivid a place, object, character, or group. Description is one of four rhetorical modes, along with exposition, argumentation, and narration. In practice it would be difficult to write literature that drew on just one of the four basic modes.',
-      href: 'https://lanport.ch/lan2k22',
-      start: '1996-12-17T03:24:0',
-      end: '1995-12-17T03:24:0',
-      organiser: 'Lanport',
-    },
-    {
-      title: 'Lan C',
-      location: 'Zegline AMK',
-      description:
-        'Description is the pattern of narrative development that aims to make vivid a place, object, character, or group. Description is one of four rhetorical modes, along with exposition, argumentation, and narration. In practice it would be difficult to write literature that drew on just one of the four basic modes.',
-      href: 'https://lanport.ch/lan2k22',
-      start: '1997-12-17T03:24:0',
-      end: '1995-12-17T03:24:0',
-      organiser: 'Lanport',
-    },
-    {
-      title: 'Lan D',
-      location: 'Zegline AMK',
-      description:
-        'Description is the pattern of narrative development that aims to make vivid a place, object, character, or group. Description is one of four rhetorical modes, along with exposition, argumentation, and narration. In practice it would be difficult to write literature that drew on just one of the four basic modes.',
-      href: 'https://lanport.ch/lan2k22',
-      start: '1998-12-17T03:24:0',
-      end: '1995-12-17T03:24:0',
-      organiser: 'Lanport',
-    },
-    {
-      title: 'Lan E',
-      location: 'Zegline AMK',
-      description:
-        'Description is the pattern of narrative development that aims to make vivid a place, object, character, or group. Description is one of four rhetorical modes, along with exposition, argumentation, and narration. In practice it would be difficult to write literature that drew on just one of the four basic modes.',
-      href: 'https://lanport.ch/lan2k22',
-      start: '1999-12-17T03:24:0',
-      end: '1995-12-17T03:24:0',
-      organiser: 'Lanport',
-    },
-    {
-      title: 'Lan F',
-      location: 'Zegline AMK',
-      description:
-        'Description is the pattern of narrative development that aims to make vivid a place, object, character, or group. Description is one of four rhetorical modes, along with exposition, argumentation, and narration. In practice it would be difficult to write literature that drew on just one of the four basic modes.',
-      href: 'https://lanport.ch/lan2k22',
-      start: '2000-12-17T03:24:0',
-      end: '1995-12-17T03:24:0',
-      organiser: 'Lanport',
-    },
-    {
-      title: 'Lan G',
-      location: 'Zegline AMK',
-      description:
-        'Description is the pattern of narrative development that aims to make vivid a place, object, character, or group. Description is one of four rhetorical modes, along with exposition, argumentation, and narration. In practice it would be difficult to write literature that drew on just one of the four basic modes.',
-      href: 'https://lanport.ch/lan2k22',
-      start: '2001-12-17T03:24:0',
-      end: '1995-12-17T03:24:0',
-      organiser: 'Lanport',
-    },
-    {
-      title: 'Lan H',
-      location: 'Zegline AMK',
-      description:
-        'Description is the pattern of narrative development that aims to make vivid a place, object, character, or group. Description is one of four rhetorical modes, along with exposition, argumentation, and narration. In practice it would be difficult to write literature that drew on just one of the four basic modes.',
-      href: 'https://lanport.ch/lan2k22',
-      start: '2002-12-17T03:24:0',
-      end: '1995-12-17T03:24:0',
-      organiser: 'Lanport',
-    },
-    {
-      title: 'Lan I',
-      location: 'Zegline AMK',
-      href: 'https://lanport.ch/lan2k22',
-      start: '2003-12-17T03:24:0',
-      end: '1995-12-17T03:24:0',
-      organiser: 'Lanport',
-    },
+    
   ],
   images: [
     {
