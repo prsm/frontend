8299bfb674b4fc497cae893e3c7b9f1ceefe02b1
jonasschultheiss <jonas.raphael.schultheiss@gmail.com>
2021-10-18 22:35:23 +0200

    :lipstick: fixed date and added favicon

--- components/TextSection.js
+++ components/TextSection.js
@@ -3,7 +3,7 @@ import { Typography } from '@pr1sm/refracted';
 export default function TextSection({}) {
   return (
     <div>
-      <Typography variant="pretitle" text="12 / 10 / 2021" component="div" color="red" />
+      <Typography variant="pretitle" text="18 / 10 / 2021" component="div" color="red" />
       <Typography variant="h2" text="PR1SM" component="h1" />
       <Typography variant="subtitle" text="Herzlich Willkommen auf unserer temporären Website" component="div" />
       <Typography variant="h3" text="Was ist PR1SM?" component="h2" className="mt-4" />
@@ -11,7 +11,9 @@ export default function TextSection({}) {
         Wir sind ein lokaler Kulturverein, der sich in den folgenden Bereichen einsetzt: Gaming, Informatik,
         Online/Offline Events, Lanparties und andere Aktivitäten.
       </Typography>
-      <Typography variant="h5" component="h2">Was machen wir?</Typography>
+      <Typography variant="h5" component="h2">
+        Was machen wir?
+      </Typography>
       <Typography variant="body" component="p">
         Wir organisieren regelmässig eigene Events wie Turniere in verschiedenen Games oder sogar LAN Partys. Zudem
         treffen wir uns für gesellschaftliche Aktivitäten wie z. B. einen Pokerabend. Gelegentlich nehmen wir an
